bc8ceb4df8599fb1f6c9bc71a05efbb742c42a91
Sarmad rafique <sarmadrafique040@gmail.com>
2023-05-26 10:34:28 +0500

    Component for rendering out the todos

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+import trashIcon from "./delete.svg";
+interface TodoProps {
+  title: string;
+  id: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+  handleDelete: (id: string) => void;
+}
+const TodoItem = ({
+  id,
+  title,
+  complete,
+  toggleTodo,
+  handleDelete,
+}: TodoProps) => {
+  return (
+    <div className="w-full m-5 px-2 grid grid-cols-2 gap-x-20 text-start lg:w-1/2 lg:text-center lg:mx-auto">
+      <section>
+        <input
+          type="checkbox"
+          className="peer cursor-pointer"
+          defaultChecked={complete}
+          id={id}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        <label
+          htmlFor={id}
+          className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500 mx-2 text-sm"
+        >
+          {title}
+        </label>
+      </section>
+      <section>
+        <form action="/">
+          <button
+            className="mx-5 rounded-md py-1 px-2 bg-gray-100 hover:bg-gray-300"
+            onClick={(e) => handleDelete(id)}
+          >
+            <Image src={trashIcon} alt="Delete" className="w-[90%]" />
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default TodoItem;
